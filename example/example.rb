9037cb71a7f9ede31d473783e8bf573db2ec6d5d
Junegunn Choi <junegunn.c@gmail.com>
2011-08-02 14:30:04 +0900

    Version bump to 0.2.0

---
+++ example/example.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'parallelize'
+
+parallelize(4) do |thread_idx|
+	puts "I'm thread ##{thread_idx}" # thread_idx is zero-based
+	# ...
+end
+
+# Enumerable#peach
+(0..100).peach(4) do |elem, thread_idx|
+	puts "Thread ##{thread_idx} processing #{elem}"
+end
+
+begin
+	parallelize(4, true) do |elem, thread_idx|
+		# Each thread can complete its block even when some other threads throw exceptions
+		raise Exception.new(thread_idx) if thread_idx < 2
+		sleep 3
+	end
+rescue ParallelException => e
+	p e.exceptions
+end
+
