0b54ba0018b2d0ff4a5398a9452fc337ccf1b554
Junegunn Choi <junegunn.c@gmail.com>
2011-08-02 14:32:40 +0900

    num_threads

--- lib/parallelize.rb
+++ lib/parallelize.rb
@@ -3,9 +3,9 @@ require 'parallelize/enumerable_ext'
 
 # Execute the given block with multiple threads.
 # @return [Array] Threads.
-# @param [Fixnum] n_thr Number of concurrent threads
+# @param [Fixnum] num_threads Number of concurrent threads
 # @param [Boolean] collect_exceptions If true, waits for all threads to complete even in case of exception, and throws ParallelException at the end. If false exception is immediately thrown.
-def parallelize n_thr, collect_exceptions = false, &block
-	n_thr.times.map.peach(n_thr, collect_exceptions, &block)
+def parallelize num_threads, collect_exceptions = false, &block
+	num_threads.times.map.peach(num_threads, collect_exceptions, &block)
 end
 
--- lib/parallelize/enumerable_ext.rb
+++ lib/parallelize/enumerable_ext.rb
@@ -1,11 +1,11 @@
 module Enumerable
 	# Divides the Enumerable objects into pieces and execute with multiple threads
 	# @return [Array] Threads.
-	# @param [Fixnum] n_thr Number of concurrent threads
+	# @param [Fixnum] num_threads Number of concurrent threads
 	# @param [Boolean] collect_exceptions If true, waits for all threads to complete even in case of exception, and throws ParallelException at the end. If false exception is immediately thrown.
-	def peach n_thr, collect_exceptions = false
+	def peach num_threads, collect_exceptions = false
 		threads = []
-		self.each_slice((self.count{true} / n_thr.to_f).ceil) do |slice|
+		self.each_slice((self.count{true} / num_threads.to_f).ceil) do |slice|
 			threads << 
 				Thread.new(slice, threads.length) { |my_slice, thread_idx|
 					my_slice.each { |e| yield e, thread_idx }
